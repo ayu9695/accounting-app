590ba9bb57815f1704ff78b0d1fc9a44bbafc554
ayu9695 <ayushiparihar3112@gmail.com>
2025-07-21 20:37:39 +0530

    response cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -25,7 +25,8 @@ exports.login = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
     //   secure: process.env.NODE_ENV === 'production',
-      sameSite: 'Strict',
+      sameSite: 'None',
+      secure: true,
       maxAge: 24 * 60 * 60 * 1000 // 1 day
     });
 
